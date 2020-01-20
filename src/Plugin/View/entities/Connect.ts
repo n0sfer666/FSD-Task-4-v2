2a21df18db2e28fa0d3f092c3fb627dbb8033672
Khasanov Petr <pietroismad@gmail.com>
2020-01-20 15:33:49 +0300

    fix(View): wrong variables in function
    In Connect entity was useed wrong variables (func set_connect_position)

--- src/Plugin/View/entities/Connect.ts
+++ src/Plugin/View/entities/Connect.ts
@@ -16,13 +16,13 @@ export class Connect extends Helper {
         let start: number = position_start_safe_int / this.TO_CONNECT_UPDATE;
         let end: number = position_end_safe_int / this.TO_CONNECT_UPDATE;
 
-        let style: string = position_start_safe_int === 0
+        let style: string = start === 0
                 ? this.orientation === 'horizontal'
-                    ? `width: ${position_end_safe_int}%;`
-                    : `height: ${position_end_safe_int}%;`
+                    ? `width: ${end}%;`
+                    : `height: ${end}%;`
                 : this.orientation === 'horizontal'
-                    ? `left: ${position_start_safe_int}%; width: ${(position_end_safe_int - position_start_safe_int)}%;`
-                    : `top: ${position_start_safe_int}%; height: ${(position_end_safe_int - position_start_safe_int)}%;`;
+                    ? `left: ${start}%; width: ${(end - start)}%;`
+                    : `top: ${start}%; height: ${(end - start)}%;`;
 
         this.element.setAttribute('style', style);
     }
